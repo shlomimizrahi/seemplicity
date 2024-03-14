5092e88604ebf613116bb4dc6f4816b3fc550417
myrepo <50696953+shlomimizrahi@users.noreply.github.com>
2024-03-14 18:28:33 +0200

    Update server.py

--- server.py
+++ server.py
@@ -6,12 +6,13 @@ from typing import Dict, Any
 from uuid import uuid4, UUID
 
 from fastapi import BackgroundTasks
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi import HTTPException
 from pydantic import BaseModel
 from tortoise import Tortoise
 
 from models import TaskResult
+from state_manager import create_or_update_task, DONE
 from task_manager import execute_task
 
 logging.basicConfig(level=logging.DEBUG)
@@ -39,10 +40,11 @@ async def shutdown() -> None:
 
 
 @app.post("/task/")
-async def create_task(background_tasks: BackgroundTasks, payload: Dict[str, Any]) -> Dict[str, str]:
+async def create_task(background_tasks: BackgroundTasks, payload: Dict[str, Any], request: Request) -> Dict[str, str]:
     """Endpoint to create and enqueue a task.
 
     Args:
+        request (Request): The request context, injected by FastAPI
         background_tasks (BackgroundTasks): The FastAPI background task manager.
         payload (Dict): A dictionary containing "task_name" and "parameters".
 
@@ -50,12 +52,15 @@ async def create_task(background_tasks: BackgroundTasks, payload: Dict[str, Any]
         Dict: A dictionary containing the UUID of the created task.
     """
     try:
+        client_address = request.client.host
         task_name = payload.get("task_name")
         parameters = payload.get("parameters")
         if not task_name or not parameters:
             raise HTTPException(status_code=400, detail="task_name and parameters are required")
 
         task_id: UUID = uuid4()
+        await create_or_update_task(task_id=str(task_id), task_name=task_name, parameters=parameters,
+                                    client_address=client_address)
         background_tasks.add_task(execute_task, task_id=str(task_id), task_name=task_name, parameters=parameters)
         return {"task_id": str(task_id)}
     except Exception as e:
@@ -67,7 +72,7 @@ class TaskResultResponse(BaseModel):
     task_id: str
     task_name: str
     parameters: dict
-    result: str
+    result: str = {}
     timestamp: datetime
 
 
@@ -85,10 +90,15 @@ async def get_task_result(task_id: str) -> TaskResultResponse:
     if not task_result:
         raise HTTPException(status_code=404, detail="Task not found")
 
-    return TaskResultResponse(
+    task_result_response: TaskResultResponse = TaskResultResponse(
         task_id=task_id,
         task_name=task_result.task_name,
         parameters=task_result.parameters,
-        result=task_result.result,
-        timestamp=task_result.timestamp
+        timestamp=task_result.timestamp,
+        state=task_result.state,
     )
+
+    if task_result.state == DONE:
+        task_result_response.result = task_result.result
+
+    return task_result_response
