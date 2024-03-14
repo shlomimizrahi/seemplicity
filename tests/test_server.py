67c10d9387bf48bcf7f9493d5222187cece5ba9c
myrepo <50696953+shlomimizrahi@users.noreply.github.com>
2024-03-14 16:50:34 +0200

    Update test_server.py

--- tests/test_server.py
+++ tests/test_server.py
@@ -4,7 +4,7 @@ from server import app
 
 
 @pytest.mark.asyncio
-async def test_create_task_sum():
+async def test_create_task_sum() -> None:
     async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
         response = await ac.post("/task/", json={"task_name": "Sum", "parameters": {"a": 5, "b": 7}})
     assert response.status_code == 200
@@ -17,7 +17,7 @@ async def test_create_task_sum():
 
 
 @pytest.mark.asyncio
-async def test_create_task_concat():
+async def test_create_task_concat() -> None:
     async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
         response = await ac.post("/task/", json={"task_name": "Concat",
                                                  "parameters": {"str1": "Hello", "str2": " ", "str3": "World"}})
@@ -31,7 +31,7 @@ async def test_create_task_concat():
 
 
 @pytest.mark.asyncio
-async def test_get_task_result_not_found():
+async def test_get_task_result_not_found() -> None:
     async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
         response = await ac.get("/task/non_existing_task_id")
     assert response.status_code == 404
